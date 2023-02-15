aaa3211a9f3f6bc5a10054e6f52d1d40a5c27be7
Mathmaid <997664460@qq.com>
2023-02-15 17:23:44 +0800

    Update AMS

--- AMS_booklist/main.py
+++ AMS_booklist/main.py
@@ -1,4 +1,6 @@
+import pandas
 import requests
+import json
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15'
@@ -10,9 +12,22 @@ data = {
     # MIME类型: application/x-www-form-urlencoded; charset=UTF-8
     'filterString': 'Series=GSM',
     'pageNumber': 1,
+    # 总书籍数量，需要随时更新
     'numPerPage': 230,
     'sortBy': 'DATE_DESC'
 }
 
 response = requests.post(url, data=data, headers=headers)
 
+# 进行数据清洗
+
+data = json.loads(response.text)
+data = data['Products']
+df = pandas.DataFrame(data)
+df1 = df[['CopyrightYear','ProductCode','RelatedCustomers','Title','Subtitle','ProductDescription']]
+
+# TODO 将信息中的作者信息和机构信息提取出来
+df1.columns = ['出版年','序列号','信息','标题','副标题','简介']
+
+
+df1.to_csv('booklist.csv')
